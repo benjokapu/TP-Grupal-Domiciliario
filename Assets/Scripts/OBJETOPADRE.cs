c04cad37b99189686b5404da122413ea5c8fa6b0
benjokapu <144476747+benjokapu@users.noreply.github.com>
2024-06-28 09:27:28 -0300

    Make objects rotate

---
+++ Assets/Scripts/OBJETOPADRE.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OBJETOPADRE : MonoBehaviour
+{
+    public Vector3 rotationSpeed = new Vector3(0,1,0);
+
+    void Update()
+    {
+        transform.Rotate(rotationSpeed);
+    }
+}
